5f4005c015b92b1f5f0889966354dbea3128f646
Kogehame <kogehame@gmail.com>
2015-11-11 16:54:57 +0100

    Start

---
+++ Teszt/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teszt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ez egy teszt program! Árvíztűrő tükörfúrógép");
+            Console.ReadLine();
+        }
+    }
+}
